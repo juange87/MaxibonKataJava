0877b77a54b47b2c24c2cf4303f46fbec893e923
Juan Jesús Fernández Madrid <juange@joinverse.com>
2017-03-25 12:32:13 +0100

    add setup method

--- src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -4,17 +4,23 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class)
 public class KarumiHQsProperties {
+    private KarumiHQs karumiHQs;
+
+    @Before
+    public void setUp() throws Exception {
+        karumiHQs = new KarumiHQs();
+    }
 
     @Property()
     public void theNumberOfMaxibonsLeftIsBiggerThanTwo(@From(HungryDevelopersGenerator.class) Developer developer) {
         System.out.println("Comiendo maxibons: " + developer.toString());
-        KarumiHQs karumiHQs = new KarumiHQs();
         karumiHQs.openFridge(developer);
         final int maxibonsLeft = karumiHQs.getMaxibonsLeft();
         System.out.println("MAXIBONS LEFT: " + maxibonsLeft);
